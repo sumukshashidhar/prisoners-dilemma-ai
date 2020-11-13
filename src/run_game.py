31cedda3d514d89082992e7d0fcfbe0eb6039786
sumukshashidhar <sumukuuu@gmail.com>
2020-11-13 17:46:41 +0530

    added the basic game

--- src/run_game.py
+++ src/run_game.py
@@ -1,3 +1,13 @@
 """
 Write docs
 """
+from game import *
+
+
+if __name__ == "__main__":
+    instance = GameEngine()
+    print("Welcome to the game")
+    print("There are two playes")
+    instance.play_move(1, "red")
+    instance.play_move(0, "blue")
+    print(instance.determine_outcome())
