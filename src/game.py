2b575e5826d2f76b2f58bfa7e1f544e21292df1a
sumukshashidhar <sumukuuu@gmail.com>
2020-11-13 17:05:17 +0530

    added some more basic structure

--- src/game.py
+++ src/game.py
@@ -1,12 +1,20 @@
-class Player
+class Player:
+    def __init__(self, idx):
+        self.color = idx
+        self.move = None
 
+    def get_move(self):
+        return self.move
 
 
 class GameEngine:
-
     def __init__(self):
-        self.blue_player = {}
-        self.red_player = {}
+        self.blue_player = Player("blue")
+        self.red_player = Player("Red")
         return
 
     def start_game(self):
+        print(self.display_message)
+
+    def determine_outcome(self):
+        move1 =
