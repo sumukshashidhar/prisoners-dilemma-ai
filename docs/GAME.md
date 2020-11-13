fd0c4c36790629d2900f551da275675161637f9a
sumukshashidhar <sumukuuu@gmail.com>
2020-11-13 17:28:36 +0530

    added some documentation

---
+++ docs/GAME.md
@@ -0,0 +1,10 @@
+# Moves
+
+The moves are stored as 1 or 0. Since we are cynical developers, we believe that `1` means to defect, and `0` means to not defect.
+
+
+# Outcomes
+
+- If both players defect, the outcome will be coded as `1` 
+- If one player defects, the outcome will be coded as `0.5` and the player id number is returned.
+- If neither player defects, the outcome will be coded as `0`. 
--- src/game.py
+++ src/game.py
@@ -7,19 +7,23 @@ class Player:
 class GameEngine:
     def __init__(self):
         self.blue_player = Player("blue")
-        self.red_player = Player("Red")
+        self.red_player = Player("red")
         return
 
     def start_game(self):
         print(self.display_message)
 
+    def play_move(self, move, id):
+
+
     def determine_outcome(self):
         move1 = self.blue_player.move
         move2 = self.red_player.move
         if move1 and move2:
-            return 1.0
-        elif (move1 and not move2) or (not move1 and move2):
-            return 0.5
+            return 1.0, None
+        elif move1 and not move2:
+            return 0.5, self.blue_player.color
+        elif not move1 and move2:
+            return 0.5, self.red_player.color
         else:
-            return 0
-        
+            return 0, None
