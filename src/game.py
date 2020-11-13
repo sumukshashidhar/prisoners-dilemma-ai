bf98ff1c6028fce9cc80359ee130325e29609a78
sumukshashidhar <sumukuuu@gmail.com>
2020-11-13 17:17:22 +0530

    added outcome determination

--- src/game.py
+++ src/game.py
@@ -3,9 +3,6 @@ class Player:
         self.color = idx
         self.move = None
 
-    def get_move(self):
-        return self.move
-
 
 class GameEngine:
     def __init__(self):
@@ -17,4 +14,12 @@ class GameEngine:
         print(self.display_message)
 
     def determine_outcome(self):
-        move1 =
+        move1 = self.blue_player.move
+        move2 = self.red_player.move
+        if move1 and move2:
+            return 1.0
+        elif (move1 and not move2) or (not move1 and move2):
+            return 0.5
+        else:
+            return 0
+        
